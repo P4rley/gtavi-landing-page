78946d2da48e1a9e29b4e88eb327dc1d61743c0d
Adwitya Hersa <adwihersa@gmail.com>
2025-08-22 19:10:43 +0700

    Finalize with smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,24 @@ import Jason from "./sections/Jason";
 import SecondVideo from "./sections/SecondVideo";
 import Lucia from "./sections/Lucia";
 import PostCard from "./sections/PostCard";
+import Final from "./sections/Final";
+import Outro from "./sections/Outro";
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <main>
       <Navbar />
@@ -22,6 +36,8 @@ const App = () => {
       <SecondVideo />
       <Lucia />
       <PostCard />
+      <Final />
+      <Outro />
     </main>
   );
 };
--- src/sections/Final.jsx
+++ src/sections/Final.jsx
@@ -2,17 +2,35 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { useRef } from "react";
 
-const PostCard = () => {
+const Final = () => {
   const videoRef = useRef(null);
 
   useGSAP(() => {
+    gsap.set(".final", { opacity: 0 });
+
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: ".final",
+        start: "top top",
+        end: "90% top",
+        scrub: true,
+        pin: true,
+      },
+    });
+
     const tl = gsap.timeline({
       scrollTrigger: {
-        trigger: ".post-card",
-        start: "top center",
-        end: "bottom center",
+        trigger: ".final",
+        start: "top 80%",
+        end: "90% top",
         scrub: true,
       },
+    });
+
+    tl.to(".final", {
+      opacity: 1,
+      duration: 2,
+      ease: "power1.inOut",
     });
 
     videoRef.current.onloadedmetadata = () => {
@@ -26,30 +44,22 @@ const PostCard = () => {
         "<"
       );
     };
-  });
+  }, []);
 
   return (
-    <section className="post-card">
-      <div className="animated-gradient-bg" />
-
-      <div className="post-card-wrapper group hover:rotate-1 hover:-[1.02] transition-duration-700 ease-in-out">
-        <img src="/images/overlay.webp" alt="" />
-
+    <section className="final">
+      <div className="final-content size-full">
         <video
+          src="/videos/output3.mp4"
           ref={videoRef}
-          src="/videos/postcard-vd.mp4"
           muted
-          autoPlay
           playsInline
           preload="auto"
+          className="size-full object-cover"
         ></video>
-
-        <button className="group-hover:bg-yellow transition duration-700">
-          Explore Leonida Keys
-        </button>
       </div>
     </section>
   );
 };
 
-export default PostCard;
+export default Final;
